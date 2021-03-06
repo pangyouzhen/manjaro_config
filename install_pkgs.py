8d6f9ecb438efa45a52ad97b04b9d6b35ff434d4
pangyouzhen <pangyouzhen@live.com>
2021-03-06 10:41:00 +0800

    fixed

--- install_pkgs.py
+++ install_pkgs.py
@@ -1,5 +1,14 @@
 import subprocess
 
+# 创建data文件夹
+mkdir_data = "mkdir /data/conda"
+mkdir_data_docker = "mkdir /data/docker"
+mkdir_data_ = subprocess.call(mkdir_data.split())
+mkdir_data_docker_ = subprocess.call(mkdir_data_docker.split())
+if mkdir_data_docker_ != 0:
+    print("创建文件失败")
+print("finish replace source")
+
 # 替换整体源
 source = "echo 'Server=https://mirrors.tuna.tsinghua.edu.cn/manjaro/stable/$repo/$arch' > /etc/pacman.d/mirrorlist"
 update_source = subprocess.call(source.split())
@@ -17,12 +26,20 @@ print("finish update system")
 # 安装软件
 pacman_pkgs = ["git", "gvim",
                "fcitx", "fcitx-configtool", "fcitx-rime",
-               "you-get", "community/pycharm-community-edition",
+               "community/pycharm-community-edition",
                "docker",
                "yay"]
 install_cmd = "pacman -S %s"
 for i in pacman_pkgs:
     install = install_cmd % (i)
+    ins = subprocess.call(install.split())
+    if ins != 0:
+        print("安装软件失败 %s" % i)
+
+yay_pkgs = ["you-get", ]
+yay_install_cmd = "yay -S %s"
+for i in yay_pkgs:
+    install = yay_install_cmd % (i)
     ins = subprocess.call(install.split())
     if ins != 0:
         print("安装软件失败 %s" % i)
@@ -34,3 +51,12 @@ if clone_config != 0:
     print("下载配置文件失败")
 
 #  更新配置文件
+
+
+# 好像不需要再进行添加了
+# export GTK_IM_MODULE=fcitx
+# export QT_IM_MODULE=fcitx
+# export XMODIFIERS=@im=fcitx
+
+
+# sudo cp ./daemon.json /etc/docker/
--- readme.md
+++ readme.md
@@ -1,28 +1,7 @@
-轻点单击：
+manjaro 自动化安装软件和更改配置脚本
+
 sudo gedit /etc/X11/xorg.conf.d/30-touchpad.conf
-输入法配置
+
 sudo gedit ~/.xprofile
-合上盖子休眠
-
-sudo vim /etc/systemd/logind.conf
-sudo systemctl restart systemd-logind
-docker 配置
-sudo cp ./daemon.json /etc/docker/
-
-安装软件
-sudo pacman -S community/pycharm-community-edition
-sudo yay -S you-get 
-
 
 mv ./default.yaml ~/.config/fcitx/rime/build/
-
-
-xmodmap -e 'keycode  68 = Tab ISO_Left_Tab Tab ISO_Left_Tab'
-xmodmap -e 'keycode  67 = w W w W'
-
-sudo cp mirrorlist /etc/pacman.d/
-
-pycharm 无法输入中文 fcitx 输入法
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
