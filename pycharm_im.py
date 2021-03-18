d22bdc6f703a92ac0385eb2f833227551b65e2de
pangyouzhen <pangyouzhen@live.com>
2021-03-18 17:12:39 +0800

    pycharm_im

--- pycharm_im.py
+++ pycharm_im.py
@@ -1,18 +1,15 @@
-import shlex
-from typing import Union
-from subprocess import run, CompletedProcess
 import re
+from subprocess import run
 
-cat_cmd = "cat /tmp/test.txt"
-cat_out: Union[CompletedProcess, CompletedProcess[bytes]] = run(shlex.split(cat_cmd),
-                                                                capture_output=True)
-output = run(['grep', '-n', 'Run'], input=cat_out.stdout, capture_output=True)
-o = (output.stdout.decode("utf-8"))
+#  对pycharm 增加输入法的脚本
+cat_cmd = "cat /usr/share/pycharm/bin/pycharm.sh"
+cat_out = run([cat_cmd], shell=True, capture_output=True)
+output = run(['grep -n  Run'], input=cat_out.stdout, capture_output=True, shell=True)
+o = output.stdout.decode("utf-8")
 g = re.match("\d+", o).group()
-print(g)
-#TODO
-sed_cmd = "sed -i '%s;export GTK_IM_MODULE=fcitx' /tmp/pycharm.sh" % (int(g) - 2)
-print(sed_cmd)
-a = run(["sed", " -i ", r'%s;export GTK_IM_MODULE=fcitx' % (int(g) - 2),
-         "/tmp/pycharm.sh"], capture_output=True, shell=True)
-print(a.stdout)
+sed_cmd = "sed -i '%siexport GTK_IM_MODULE=fcitx' /usr/share/pycharm/bin/pycharm.sh" % (int(g) - 2)
+sed_cmd2 = "sed -i '%siexport QT_IM_MODULE=fcitx' /usr/share/pycharm/bin/pycharm.sh" % (int(g) - 2)
+sed_cmd3 = "sed -i '%siexport XMODIFIERS=@im=fcitx' /usr/share/pycharm/bin/pycharm.sh" % (int(g) - 2)
+a = run([sed_cmd], shell=True, capture_output=True)
+ab = run([sed_cmd2], shell=True, capture_output=True)
+abc = run([sed_cmd3], shell=True, capture_output=True)
