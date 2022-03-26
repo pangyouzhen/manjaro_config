5a3acb08cc7432c308e212aa314ab96a210aa37d
pang <pangyouzhen@live.com>
2022-03-26 13:31:04 +0800

    fixed

--- manjaro.sh
+++ manjaro.sh
@@ -1,8 +1,11 @@
 pacman-mirrors -c China && sudo pacman -Syuu --noconfirm && sudo pacman -S --noconfirm  yay git
 cat pkgs.txt | xargs yay -S --noconfirm
-mv xfce4-keyboard-shortcuts.xml ~/config/xfce4/xfconf/xfce-perchannel-xml/
+# 快捷键设置
+mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+mv xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 mv .pip ~
 mv .condarc ~
+# 字体设置
 mv fonts.conf ~/.config/fontconfig/
 python3.8 get-pip.py
 sudo vboxreload
