93e522a0074c00ed8a348c6ea26d1b2f589f312a
pang <pangyouzhen@live.com>
2022-03-26 20:29:39 +0800

    fixed

--- manjaro.sh
+++ manjaro.sh
@@ -1,12 +1,19 @@
 pacman-mirrors -c China && sudo pacman -Syuu --noconfirm && sudo pacman -S --noconfirm  yay git
 cat pkgs.txt | xargs yay -S --noconfirm
+
+
 # 快捷键设置
 mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 mv xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+# python源
 mv .pip ~
 mv .condarc ~
 # 字体设置
 mv fonts.conf ~/.config/fontconfig/
+
+# 触摸板设置
+mv 30-touchpad.conf  /etc/X11/xorg.conf.d/
+
 python3.8 get-pip.py
 pip3.8 install virutalenv
 sudo vboxreload
--- readme.md
+++ readme.md
@@ -10,15 +10,3 @@ mv ./default.yaml ~/.config/fcitx/rime/build/
 1. settings.xml maven配置文件
 1. daemon.json docker配置文件
 1. default.yaml 输入法fcitx-rime配置文件
-1.
-
-wechat 输入法
-
-```
-env locale=zh_CN
-
-export XIM="fcitx"
-export XMODIFIERS="@im=fcitx"
-export GTK_IM_MODULE="fcitx"
-export QT_IM_MODULE="fcitx"
-```
